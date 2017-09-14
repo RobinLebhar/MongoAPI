af154f586178660303d6cd98c44c151c534921a0
Robin Lebhar <robin_lebhar@hotmail.fr>
2017-09-13 22:19:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const server = express();
+const express = require('express');
+
+const server = express();
+
+server.get('/bonjour',(req,res)=>{
+    res.send({result:'Un resultat'})
+});
 
 server.listen(3050, () =>{
     console.log("Ecoute sur le port 3050");
