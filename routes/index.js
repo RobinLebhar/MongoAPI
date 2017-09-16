5af64cc2be84d8646df2508f00a0d34132c3a7fc
Robin Lebhar <robin_lebhar@hotmail.fr>
2017-09-15 22:10:51 -0400

    model

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 UserController = require('../controllers/user-controller');
 module.exports = (server) => {
     // Envoi les parametres req et res de facon cachée a votre fonction.
-    server.get('/users',UserController.getUsers);
-    server.get('/user/:id',UserController.getUser);
+    server.get('/users',UserController.readAll);
+    server.get('/user/:id',UserController.read);
+    server.post('/user',UserController.create);
 }
 
 
